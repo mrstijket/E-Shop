d7c567c44c847e28dd8317a14ec634fb5262f605
Mr.Stijket <mr.stijket@gmail.com>
2023-10-30 12:03:10 +0300

    rating icons and database logic

---
+++ products.js
@@ -0,0 +1,257 @@
+const products = [{
+  id: 1,
+  owner: "2kye",
+  image: "img/products/f1.jpg",
+  name: "Renkli Gömlek",
+  rating: {
+    stars: 5,
+    count: 10
+  },
+  priceCents: 49900,
+  keywords: [
+    "gömlek",
+    "renkli",
+    "yaz"
+  ]
+},
+{
+  id: 2,
+  owner: "2kye",
+  image: "img/products/f2.jpg",
+  name: "Yaprak Gömlek",
+  rating: {
+    stars: 4,
+    count: 17
+  },
+  priceCents: 49900,
+  keywords: [
+    "gömlek",
+    "yaprak",
+    "yaz"
+  ]
+},
+{
+  id: 3,
+  owner: "2kye",
+  image: "img/products/f3.jpg",
+  name: "Çiçekli Gömlek",
+  rating: {
+    stars: 4,
+    count: 45
+  },
+  priceCents: 49900,
+  keywords: [
+    "gömlek",
+    "çiçekli",
+    "yaz"
+  ]
+},
+{
+  id: 4,
+  owner: "2kye",
+  image: "img/products/f4.jpg",
+  name: "Lavanta Gömlek",
+  rating: {
+    stars: 5,
+    count: 24
+  },
+  priceCents: 49900,
+  keywords: [
+    "gömlek",
+    "lavanta",
+    "yaz"
+  ]
+},
+{
+  id: 5,
+  owner: "2kye",
+  image: "img/products/f5.jpg",
+  name: "Kadife Gömlek",
+  rating: {
+    stars: 4,
+    count: 11
+  },
+  priceCents: 49900,
+  keywords: [
+    "gömlek",
+    "kadife",
+    "yaz"
+  ]
+},
+{
+  id: 6,
+  owner: "2kye",
+  image: "img/products/f6.jpg",
+  name: "Kadife Ceket",
+  rating: {
+    stars: 5,
+    count: 6
+  },
+  priceCents: 49900,
+  keywords: [
+    "ceket",
+    "kadife",
+    "yaz"
+  ]
+},
+{
+  id: 7,
+  owner: "2kye",
+  image: "img/products/f7.jpg",
+  name: "Çiçekli Pantolon",
+  rating: {
+    stars: 4,
+    count: 4
+  },
+  priceCents: 49900,
+  keywords: [
+    "pantolon",
+    "çiçekli",
+    "yaz"
+  ]
+},
+{
+  id: 8,
+  owner: "2kye",
+  image: "img/products/f8.jpg",
+  name: "Tribal Bluz",
+  rating: {
+    stars: 5,
+    count: 9
+  },
+  priceCents: 49900,
+  keywords: [
+    "tribal",
+    "bluz",
+    "yaz"
+  ]
+},
+{
+  id: 9,
+  owner: "2kye",
+  image: "img/products/n1.jpg",
+  name: "Mavi Gömlek",
+  rating: {
+    stars: 5,
+    count: 16
+  },
+  priceCents: 49900,
+  keywords: [
+    "gömlek",
+    "mavi",
+    "günlük"
+  ]
+},
+{
+  id: 10,
+  owner: "2kye",
+  image: "img/products/n2.jpg",
+  name: "Kareli Gömlek",
+  rating: {
+    stars: 5,
+    count: 10
+  },
+  priceCents: 49900,
+  keywords: [
+    "gömlek",
+    "kareli",
+    "günlük"
+  ]
+},
+{
+  id: 11,
+  owner: "2kye",
+  image: "img/products/n3.jpg",
+  name: "Beyaz Gömlek",
+  rating: {
+    stars: 5,
+    count: 10
+  },
+  priceCents: 49900,
+  keywords: [
+    "gömlek",
+    "beyaz",
+    "günlük"
+  ]
+},
+{
+  id: 12,
+  owner: "2kye",
+  image: "img/products/n4.jpg",
+  name: "Asker Gömlek",
+  rating: {
+    stars: 5,
+    count: 10
+  },
+  priceCents: 49900,
+  keywords: [
+    "gömlek",
+    "asker",
+    "yaz"
+  ]
+},
+{
+  id: 13,
+  owner: "2kye",
+  image: "img/products/n5.jpg",
+  name: "Kot Gömlek",
+  rating: {
+    stars: 5,
+    count: 10
+  },
+  priceCents: 49900,
+  keywords: [
+    "gömlek",
+    "kot",
+    "günlük"
+  ]
+},
+{
+  id: 14,
+  owner: "2kye",
+  image: "img/products/n6.jpg",
+  name: "Çizgili Şort",
+  rating: {
+    stars: 5,
+    count: 10
+  },
+  priceCents: 49900,
+  keywords: [
+    "şort",
+    "çizgili",
+    "yaz"
+  ]
+},
+{
+  id: 15,
+  owner: "2kye",
+  image: "img/products/n7.jpg",
+  name: "Kargo Gömlek",
+  rating: {
+    stars: 5,
+    count: 10
+  },
+  priceCents: 49900,
+  keywords: [
+    "gömlek",
+    "kargo",
+    "günlük"
+  ]
+}, {
+  id: 16,
+  owner: "2kye",
+  image: "img/products/n8.jpg",
+  name: "Bol Kesim Gömlek",
+  rating: {
+    stars: 5,
+    count: 10
+  },
+  priceCents: 49900,
+  keywords: [
+    "gömlek",
+    "bol kesim",
+    "yaz"
+  ]
+}];
+
+export default products;
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import products from "./products.js";
+
 // Script for navigation bar
 const bar = document.getElementById('bar');
 const nav = document.getElementById('navbar');
@@ -14,3 +16,28 @@ if (close) {
     nav.classList.remove('active');
   })
 }
+
+let productsHTML = '';
+
+products.forEach((product) => {
+  productsHTML += `
+    <div class="pro">
+      <img src="${product.image}">
+      <div class="des">
+        <span>${product.owner}</span>
+        <h5>${product.name}</h5>
+        <div class="star"> 
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+          <i class="fas fa-star"></i>
+        </div>
+        <h4>${(product.priceCents / 100).toFixed(2)} ₺</h4>
+      </div>
+      <a href="#"><i class="fal fa-shopping-cart cart"></i></a>
+    </div>
+  `;
+});
+
+document.querySelector('.pro-container').innerHTML = productsHTML;
